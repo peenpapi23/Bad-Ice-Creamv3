c77f473a908793f1add9e89cba347cf65936da95
Juan Diego Melo Suarez <98196877+peenpapi23@users.noreply.github.com>
2025-12-03 13:11:42 -0500

    Implement object collision detection method
    Added a new method to check collisions with objects/fruits.

--- src/dominio/Colision.java
+++ src/dominio/Colision.java
@@ -10,15 +10,14 @@ public class Colision {
         this.panel = panel;
     }
 
+    // Método para colisiones con paredes (YA LO TENÍAS)
     public void check(Entidad entidad) {
 
-        // Hitbox del jugador dentro del mundo
         int izquierda = entidad.mundoX + entidad.areaSolida.x;
         int derecha = entidad.mundoX + entidad.areaSolida.x + entidad.areaSolida.width;
         int arriba = entidad.mundoY + entidad.areaSolida.y;
         int abajo = entidad.mundoY + entidad.areaSolida.y + entidad.areaSolida.height;
 
-        // Convertir a coordenadas de tiles
         int izquierdaCol = izquierda / panel.originalTamañoPixel;
         int derechaCol = derecha / panel.originalTamañoPixel;
         int arribaFila = arriba / panel.originalTamañoPixel;
@@ -27,7 +26,6 @@ public class Colision {
         int tile1, tile2;
 
         switch(entidad.direccion) {
-
         case "arriba":
             arribaFila = (arriba - entidad.velocidad) / panel.originalTamañoPixel;
             tile1 = panel.nivel.mapaNivel1[izquierdaCol][arribaFila];
@@ -36,7 +34,6 @@ public class Colision {
                 entidad.colisionOn = true;
             }
             break;
-
         case "abajo":
             abajoFila = (abajo + entidad.velocidad) / panel.originalTamañoPixel;
             tile1 = panel.nivel.mapaNivel1[izquierdaCol][abajoFila];
@@ -45,7 +42,6 @@ public class Colision {
                 entidad.colisionOn = true;
             }
             break;
-
         case "izquierda":
             izquierdaCol = (izquierda - entidad.velocidad) / panel.originalTamañoPixel;
             tile1 = panel.nivel.mapaNivel1[izquierdaCol][arribaFila];
@@ -54,7 +50,6 @@ public class Colision {
                 entidad.colisionOn = true;
             }
             break;
-
         case "derecha":
             derechaCol = (derecha + entidad.velocidad) / panel.originalTamañoPixel;
             tile1 = panel.nivel.mapaNivel1[derechaCol][arribaFila];
@@ -64,5 +59,61 @@ public class Colision {
             }
             break;
         }
+    }
+
+    // Método para colisiones con objetos/frutas (EL NUEVO)
+    public int checkObjeto(Entidad entidad, boolean jugador) {
+        
+        int index = 999;
+        
+        // Verificamos si panel.obj es accesible. 
+        // Si te da error aquí, es porque no tienes acceso a 'obj' en PanelDeJuego.
+        // Asegúrate que en PanelDeJuego.java sea: public SuperObjeto obj[] = ...
+        
+        for(int i = 0; i < panel.obj.length; i++) {
+            
+            if(panel.obj[i] != null) {
+                
+                // Get entidad solid area position
+                entidad.areaSolida.x = entidad.mundoX + entidad.areaSolidaX;
+                entidad.areaSolida.y = entidad.mundoY + entidad.areaSolidaY;
+                
+                // Get object solid area position
+                panel.obj[i].areaSolida.x = panel.obj[i].mundoX + panel.obj[i].areaSolidaX;
+                panel.obj[i].areaSolida.y = panel.obj[i].mundoY + panel.obj[i].areaSolidaY;
+                
+                switch(entidad.direccion) {
+                case "arriba":
+                    entidad.areaSolida.y -= entidad.velocidad;
+                    break;
+                case "abajo":
+                    entidad.areaSolida.y += entidad.velocidad;
+                    break;
+                case "izquierda":
+                    entidad.areaSolida.x -= entidad.velocidad;
+                    break;
+                case "derecha":
+                    entidad.areaSolida.x += entidad.velocidad;
+                    break;
+                }
+                
+                if(entidad.areaSolida.intersects(panel.obj[i].areaSolida)) {
+                    if(panel.obj[i].colision == true) {
+                        entidad.colisionOn = true;
+                    }
+                    if(jugador == true) {
+                        index = i;
+                    }
+                }
+                
+                // Resetear valores
+                entidad.areaSolida.x = entidad.areaSolidaX;
+                entidad.areaSolida.y = entidad.areaSolidaY;
+                panel.obj[i].areaSolida.x = panel.obj[i].areaSolidaX;
+                panel.obj[i].areaSolida.y = panel.obj[i].areaSolidaY;
+            }
+        }
+        
+        return index;
     }
 }
